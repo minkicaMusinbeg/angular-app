007370adc3d10b49f28c7433b5b3c9e126c854ff
Loca <ajdin.locic@orbicode.com>
2022-09-25 03:30:26 +0200

    modules

--- src/app/components/list/list.module.ts
+++ src/app/components/list/list.module.ts
@@ -5,6 +5,14 @@ import { UserTableComponent } from './user-table/user-table.component';
 import { UserFormComponent } from './user-form/user-form.component';
 import { CommonModule } from '@angular/common';
 import { SharedModule } from 'src/app/shared/shared.module';
+import { MatIconModule } from '@angular/material/icon';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatSelectModule } from '@angular/material/select';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { ListRoutingModule } from './list-routing.module';
+import { MatCheckboxModule } from '@angular/material/checkbox';
 
 @NgModule({
   declarations: [
@@ -12,7 +20,8 @@ import { SharedModule } from 'src/app/shared/shared.module';
   ],
   imports: [
     SharedModule,
-    CommonModule
+    CommonModule,
+    ListRoutingModule
   ],
   providers: [],
 })
